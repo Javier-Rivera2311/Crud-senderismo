f149736c6c8522110f17d086c10e7d3a839c5e85
ilcv2000 <italo.c.velez@mail.com>
2023-10-17 16:11:15 -0300

    Fondo Formulario

--- src/pages/PublishRoutes/componentesPublicarRutas/Campos.jsx
+++ src/pages/PublishRoutes/componentesPublicarRutas/Campos.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from 'react';
+import './fondo.css'
 
 function FormularioFuncional() {
   const [nombre, setNombre] = useState('');
@@ -24,31 +25,33 @@ function FormularioFuncional() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <label>Nombre de la ruta:</label>
-        <input type="text" value={nombre} onChange={handleNombreChange} />
+    <div class="card">
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <label style={{frontsize: '2m'}}>Nombre de la ruta:</label>
+          <input type="text" value={nombre} onChange={handleNombreChange} /><br/>
 
-        <label>Descripción de la ruta:</label>
-        <textarea value={comentario} onChange={handleComentarioChange} />
+          <label>Descripción de la ruta:</label>
+          <textarea value={comentario} onChange={handleComentarioChange} /><br/>
 
-        <label>Selecciona la dificultad de la ruta:</label>
-        <select value={opcionSeleccionada} onChange={handleOpcionChange}>
-          <option value="opcion1">Principiante</option>
-          <option value="opcion2">Intermedio</option>
-          <option value="opcion3">Experto</option>
-          <option value="opcion4">Te lleva a conocer a Dios</option>
-        </select>
+          <label>Selecciona la dificultad de la ruta:</label>
+          <select value={opcionSeleccionada} onChange={handleOpcionChange}>
+            <option value="opcion1">Principiante</option>
+            <option value="opcion2">Intermedio</option>
+            <option value="opcion3">Experto</option>
+            <option value="opcion4">Te lleva a conocer a Dios</option>
+          </select><br/>
 
-        <label>Tipo de ruta:</label>
-        <select value={opcionSeleccionada} onChange={handleOpcionChange}>
-          <option value="opcion1">Rural</option>
-          <option value="opcion2">Urbana</option>
-          <option value="opcion3">Montañosa</option>
-        </select>
+          <label>Tipo de ruta:</label>
+          <select value={opcionSeleccionada} onChange={handleOpcionChange}>
+            <option value="opcion1">Rural</option>
+            <option value="opcion2">Urbana</option>
+            <option value="opcion3">Montañosa</option>
+          </select><br/>
 
-        <button type="submit">Enviar</button>
-      </form>
+          <button type="submit">Enviar</button><br/>
+        </form>
+      </div>
     </div>
   );
 }
