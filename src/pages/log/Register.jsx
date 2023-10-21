4aec9ab7e857cb3d1a909dbe6b91dc6c59c82b6e
Javier-Rivera2311 <javier.riverapi@alumnos.uv.cl>
2023-10-20 23:45:02 -0300

    ver password

--- src/pages/log/Register.jsx
+++ src/pages/log/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FaLock, FaLockOpen } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './Register.css';
 import fondo1 from "../../assets/fondos/fondo.jpg";
@@ -7,6 +8,12 @@ import fondo3 from "../../assets/fondos/rutas.jpg";
 
 const image = [fondo1, fondo2, fondo3]
 function RegisterForm() {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
   
     const changeImage = () => {
@@ -51,7 +58,7 @@ function RegisterForm() {
         </div>
         {/* phone */}
         <div className="field">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="input-icon" viewBox="0 0 16 16">
   <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
 </svg>
           <input autoComplete="off" placeholder="Enter your phone number" className="input-field" type="tel" />
@@ -59,7 +66,7 @@ function RegisterForm() {
 
         {/* email */}
         <div className="field">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="input-icon" viewBox="0 0 16 16">
   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
 </svg>
           <input autoComplete="off" placeholder="Email" className="input-field" type="email" />
@@ -69,11 +76,14 @@ function RegisterForm() {
         <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
           </svg>
-          <input autoComplete="off" placeholder="Password" className="input-field" type="password" />
+          <input placeholder="Password" className="input-field" type={showPassword? 'text':'password'} />
+          <button type="button" onClick={toggleShowPassword}>
+          {showPassword ? <FaLockOpen /> : <FaLock />}
+        </button>
         </div>
         {/* date */}
         <div className="field">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="input-icon" viewBox="0 0 16 16">
   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
 </svg>
           <input autoComplete="off" placeholder="Age" className="input-field" type="date" />
