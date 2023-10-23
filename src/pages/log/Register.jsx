3e36d18659457625d78c4701304b217b314491e1
Javier-Rivera2311 <142764215+Javier-Rivera2311@users.noreply.github.com>
2023-10-22 21:41:14 -0300

    cambios login, register

--- src/pages/log/Register.jsx
+++ src/pages/log/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { FaLock, FaLockOpen } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Register.css';
 import fondo1 from "../../assets/fondos/fondo.jpg";
 import fondo2 from "../../assets/fondos/publicar.jpg";
@@ -8,12 +8,20 @@ import fondo3 from "../../assets/fondos/rutas.jpg";
 
 const image = [fondo1, fondo2, fondo3]
 function RegisterForm() {
+  
+  const [Values, setValues] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:4000/register',Values)
+    .then(res => console.log(res))
+    .then(err => console.log(err));
 
-  const [showPassword, setShowPassword] = useState(false);
+  }
 
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
   
     const changeImage = () => {
@@ -38,51 +46,31 @@ function RegisterForm() {
         backgroundPosition: "center",
         width: "100%"
       }}>
-      <form className="form" style={{
+      <form onSubmit={handleSubmit} className="form" style={{
         maxWidth: 500
       }}>
-        <p id="heading">Register user</p>
-        {/* name */}
+        <p id="heading">Register</p>
         <div className="field">
-          <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
-          </svg>
-          <input autoComplete="off" placeholder="Name" className="input-field" type="text" />
-        </div>
-        {/* last name */}
-        <div className="field">
-          <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
-          </svg>
-          <input autoComplete="off" placeholder="Username" className="input-field" type="text" />
-        </div>
-        {/* email */}
-        <div className="field">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="input-icon" viewBox="0 0 16 16">
-  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
-</svg>
-          <input autoComplete="off" placeholder="Email" className="input-field" type="email" />
-        </div>
-        {/* password */}
-        <div className="field">
-        <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
-          </svg>
-          <input placeholder="Password" className="input-field" type={showPassword? 'text':'password'} />
-          <button type="button" onClick={toggleShowPassword}>
-          {showPassword ? <FaLockOpen /> : <FaLock />}
-        </button>
-        </div>
-        {/* date */}
-        <div className="field">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="input-icon" viewBox="0 0 16 16">
-  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-</svg>
-          <input autoComplete="off" placeholder="Age" className="input-field" type="date" />
-        </div>
+          <label htmlFor="name"><strong>Name:</strong></label>
+          <input autoComplete="off" placeholder="Enter Name" name='name'
+          onChange={e => setValues({...Values, name: e.target.value})} className="input-field" type="text" />
+          </div>
+
+          <div className="field">
+          <label htmlFor="email"><strong>email:</strong></label>
+          <input autoComplete="off" placeholder="Enter Email" name='email'
+          onChange={e => setValues({...Values, email: e.target.value})} className="input-field" type="email" />
+          </div>
+
+          <div className="field">
+          <label htmlFor="password"><strong>Password:</strong></label>
+          <input autoComplete="off" placeholder="Enter Password" name='password'
+          onChange={e => setValues({...Values, password: e.target.value})}className="input-field" type="password" />
+          </div>  
+
         {/* botones */}
         <div className="btn">
-          <button className="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Register&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+          <button type='submit' className="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Register&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
         </div>
         <Link to="/auth/login" className="button2 link-no-underline">Do you already have an account? Create</Link>
       </form>
