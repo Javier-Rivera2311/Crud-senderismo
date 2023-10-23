b52d29fc27dae1fea28b88bd7eae63474c4b78de
chumuelo97 <osvaldo.queupanlumnos.uv.cl>
2023-10-22 23:00:04 -0300

    cambios

--- src/pages/log/login.jsx
+++ src/pages/log/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaLock, FaLockOpen } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 import fondo1 from "../../assets/fondos/fondo.jpg";
 import fondo2 from "../../assets/fondos/publicar.jpg";
 import fondo3 from "../../assets/fondos/rutas.jpg";
@@ -43,7 +43,7 @@ function Login() {
       <form className="form" style={{
         maxWidth: 500
       }}>
-        <p id="heading">Login</p>
+        <p id="heading">login</p>
 
           <div className="field">
           <label htmlFor="email"><strong>email:</strong></label>
