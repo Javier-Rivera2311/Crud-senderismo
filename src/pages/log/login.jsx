38f24469ebbf7bf7e622712336d6bdfb4937b434
Javier-Rivera2311 <142764215+Javier-Rivera2311@users.noreply.github.com>
2023-10-23 22:51:14 -0300

    login

--- src/pages/log/login.jsx
+++ src/pages/log/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
-import './login.css';
+import './Login.css';
 import fondo1 from "../../assets/fondos/fondo.jpg";
 import fondo2 from "../../assets/fondos/publicar.jpg";
 import fondo3 from "../../assets/fondos/rutas.jpg";
