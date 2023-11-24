700a2cb127f4a45559ebfbec480cf6ecfac9439a
Javier-Rivera2311 <142764215+Javier-Rivera2311@users.noreply.github.com>
2023-11-24 01:09:43 -0300

    cambios

--- src/layouts/login-layout/LoginLayout.jsx
+++ src/layouts/login-layout/LoginLayout.jsx
@@ -1,10 +1,17 @@
 import { Backdrop, CircularProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet,Navigate } from "react-router-dom";
 
 function LoginLayout() {
   const [loading, isLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   useEffect(() => {
+
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+
     setTimeout(() => {
       isLoading(false);
     }, 2000);
@@ -13,9 +20,14 @@ function LoginLayout() {
   return (
     <>
       {!loading ? (
+        isAuthenticated ? (
         <>
         <Outlet />
       </>
+        ) : (
+          // Redirige al usuario a la página de inicio de sesión si no está autenticado
+          <Navigate to="/auth/Login" />
+        )
       ) : (
         <Backdrop
           sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
